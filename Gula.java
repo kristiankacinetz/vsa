d7414c8eef1f3ad87007b47299e50cc10e6ff50e
kristiankacinetz <kristian.kacinetz@gmail.com>
2014-12-08 23:55:29 +0100

    src2
    zdrojak2

---
+++ Gula.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kristián Kačinetz
+ */
+public class Gula extends AbstractUtvar {
+    
+    private int x;
+    private int y;
+    private int z;
+    private int polomer;
+    
+    public Gula(int x, int y, int z, int polomer)
+    {
+        super( x, y, z);
+        this.polomer = polomer;
+    }
+
+  
+ 
+    
+    @Override
+    public double getObjem() {
+       return  (4.0/3.0)*Math.PI*polomer*polomer*polomer;
+    }
+    
+    
+}
